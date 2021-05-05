380964245356b12db98f182b3685639d574a25f5
Eric fillipe <ericfillipework@gmail.com>
2021-05-04 22:32:21 -0300

    * Criando função recursiva para soma de numeros * Definindo funções privadas e publicas * Aprendendo a usar o operador pipe (head | tail)

---
+++ lib/sum_list.ex
@@ -0,0 +1,11 @@
+defmodule SumList do
+  def call(list)  , do: sum(list,0)
+  defp sum([], acc) do
+    acc
+  end
+
+  defp sum([head | tail], acc) do
+    acc = head + acc
+    sum(tail, acc)
+  end
+end
